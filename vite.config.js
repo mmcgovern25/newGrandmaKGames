961622e32c1174fa3539defeba5c75d26ccb3d91
Matthew McGovern <mattmcg37@gmail.com>
2025-03-31 04:26:59 +0400

    help

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/newGrandmaKGames/', // This should be the name of your repository
   plugins: [react()],
+  base: '/newGrandmaKGames/', // Add this line
 })
